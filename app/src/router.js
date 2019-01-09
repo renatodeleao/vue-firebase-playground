437fa3384bdc4036f03d25c4b276d645acec5b9d
Renato de Leão <renatodeleao@gmail.com>
2019-01-09 12:53:55 +0000

    handling githubpages SPA history mode

--- app/src/router.js
+++ app/src/router.js
@@ -7,6 +7,10 @@ Vue.use(Router);
 export default new Router({
   routes: [
     {
+      path: "*",
+      component: () => import("./views/404.vue")
+    },
+    {
       path: "/",
       name: "home",
       component: Home
